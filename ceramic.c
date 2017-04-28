68b7b8f0e04c30f18acaf1681b08b472c48dc640
Theo Kalfas <kalfastheodoros@gmail.com>
2017-04-28 12:00:48 -0500

    Partial progress on arrow keys

--- ceramic.c
+++ ceramic.c
@@ -4,11 +4,14 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /* Defines */
+
+#define CERAMIC_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -21,6 +24,7 @@
  */
 
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -31,8 +35,8 @@ struct editorConfig E;
 /* Terminal sets */
 
 void die(const char *s) {
-  //write(STDOUT_FILENO, "\x1b[2J", 4);
-  //write(STDOUT_FILENO, "\x1b[H", 3);
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   
   perror(s);
   exit(1);
@@ -66,9 +70,36 @@ char editorReadKey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-    if (nread == -1 && errno != EAGAIN) die ("read");
+    if (nread == -1 && errno != EAGAIN)
+      die ("read");
   }
-  return c;
+
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+      die("uuuuuu");
+      return '\x1b';
+    }
+
+    if (seq[0] == '[') {
+      die("lolololol");
+      switch (seq[1]) {
+        case 'A': return 'h';
+        case 'B': return 'l';
+        case 'C': return 'j';
+        case 'D': return 'k';
+      }
+    }
+
+    return '\x1b';
+  }
+  else {
+    return c;
+  }
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -116,26 +147,94 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/* Append Buffer */
+
+struct abuf {
+  char *b;
+  int length;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int length) {
+  char *new = realloc(ab->b, ab->length + length);
+  memcpy(&new[ab->length], s, length);
+  ab->b = new;
+  ab->length += length;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+
 /* Output */
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int i;
+
   for (i=0; i < E.screenrows - 1; i++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    if (i == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Ceramic text editor -- version %s", CERAMIC_VERSION);
+      if (welcomelen > E.screencols) 
+        welcomelen = E.screencols;
+
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+
+      while (padding--)
+        abAppend(ab, " ", 1);
+
+      abAppend(ab, welcome, welcomelen);
+      abAppend(ab, "\r\n", 2);
+    }
+    else {
+      abAppend(ab, "~\x1b[K\r\n", 6);
+    }
   }
-  write(STDOUT_FILENO, "~", 1); // To draw last tilde without a newline after
+  abAppend(ab, "~\x1b[K", 4);
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
+  //abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows(&ab);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
+  //abAppend(&ab, "\x1b[?25h", 3);
+
+  write(STDOUT_FILENO, ab.b, ab.length);
+  abFree(&ab);
 }
 
 /* Input Process */
+
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'h':
+      E.cx--;
+      break;
+    case 'l':
+      E.cx++;
+      break;
+    case 'j':
+      E.cy++;
+      break;
+    case 'k':
+      E.cy--;
+      break;
+  }
+}
 
 void editorProcessKeypress() {
   char c = editorReadKey();
@@ -146,12 +245,23 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+    
+    // Arrow keys
+    case 'h':
+    case 'j':
+    case 'k':
+    case 'l':
+      editorMoveCursor(c);
+      break;
   }
 }
 
 /* Initialization */
 
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSizzzz");
 }
@@ -162,7 +272,7 @@ int main() {
 
   enableRawMode();
   initEditor();
-
+  
   while (1) {
     editorRefreshScreen();
     editorProcessKeypress();
